187c61588223fd7379b7b035894d16b9e0c6dcf7
Gomding <backchida@gmail.com>
2020-11-25 02:40:47 +0900

    ApplicationConfig를 클래스에 @Bean 애너테이션을 이용해서 빈 설정을 함 @Bean 설정에서 주입받을 빈을 setBookRepository를 이용해서 넣어줌 (autowired x)

--- src/main/java/com/example/springapplicationcontext/DemoApplication.java
+++ src/main/java/com/example/springapplicationcontext/DemoApplication.java
@@ -1,9 +1,8 @@
 package com.example.springapplicationcontext;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
 
@@ -11,7 +10,9 @@ import java.util.Arrays;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class); // 이 클래스를 빈 설정으로 함
+        //ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+
         String[] beanDefinitionNames = context.getBeanDefinitionNames();
         System.out.println(Arrays.toString(beanDefinitionNames));
         BookService bookService = (BookService) context.getBean("bookService");
