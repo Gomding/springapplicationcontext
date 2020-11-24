1e5ee05750dd4a3ad2ea66b4476b27b765113894
Gomding <backchida@gmail.com>
2020-11-25 00:59:57 +0900

    application.xml 의 <bean> 을 이용한 빈등록

---
+++ src/main/java/com/example/springapplicationcontext/BookRepository.java
@@ -0,0 +1,5 @@
+package com.example.springapplicationcontext;
+
+
+public class BookRepository {
+}
--- src/main/java/com/example/springapplicationcontext/DemoApplication.java
+++ src/main/java/com/example/springapplicationcontext/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.springapplicationcontext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        System.out.println(Arrays.toString(beanDefinitionNames));
+        BookService bookService = (BookService) context.getBean("bookService");
+        System.out.println(bookService.bookRepository != null);
     }
 
 }
