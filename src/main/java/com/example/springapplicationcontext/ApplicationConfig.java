420158328ff12fa224b19c150a0ac790b3adcec2
Gomding <backchida@gmail.com>
2020-11-25 02:49:16 +0900

    ApplicationConfig(빈 설정 클래스) 에서 @ComponentScan을 이용해서 빈 생성하기

--- src/main/java/com/example/springapplicationcontext/ApplicationConfig.java
+++ src/main/java/com/example/springapplicationcontext/ApplicationConfig.java
@@ -1,20 +1,10 @@
 package com.example.springapplicationcontext;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackageClasses = DemoApplication.class)
 public class ApplicationConfig {
 
-    @Bean
-    public BookRepository bookRepository() {
-        return new BookRepository();
-    }
-
-    @Bean
-    public BookService bookService() {
-        BookService bookService = new BookService();
-        bookService.setBookRepository(bookRepository());
-        return bookService;
-    }
 }
--- src/main/java/com/example/springapplicationcontext/BookService.java
+++ src/main/java/com/example/springapplicationcontext/BookService.java
@@ -1,10 +1,12 @@
 package com.example.springapplicationcontext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookService {
 
-    //@Autowired // @Autowired 로 빈을 주입받음 @Inject 도 가능
+    @Autowired // @Autowired 로 빈을 주입받음 @Inject 도 가능
     BookRepository bookRepository;
 
     public void setBookRepository(BookRepository bookRepository) {
